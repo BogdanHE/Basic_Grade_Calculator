1d5ff2a71ea7b46845cd16aeda5437619b8a562d
Alex <Alex@DESKTOP-L01R6N3>
2022-06-03 20:37:19 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Basic_Grade_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Input1 = new System.Windows.Forms.TextBox();
+            this.InputAverage = new System.Windows.Forms.TextBox();
+            this.InputTotal = new System.Windows.Forms.TextBox();
+            this.Input2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Input3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Input4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Goudy Stout", 10.2F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Networking";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Font = new System.Drawing.Font("Goudy Stout", 10.2F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(263, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mathematics";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label3.Font = new System.Drawing.Font("Goudy Stout", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(242, 338);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Total";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Goudy Stout", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(210, 411);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(215, 31);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Average";
+            // 
+            // Input1
+            // 
+            this.Input1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Input1.Location = new System.Drawing.Point(453, 52);
+            this.Input1.Name = "Input1";
+            this.Input1.Size = new System.Drawing.Size(100, 27);
+            this.Input1.TabIndex = 4;
+            this.Input1.TextChanged += new System.EventHandler(this.Input1_TextChanged);
+            // 
+            // InputAverage
+            // 
+            this.InputAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputAverage.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.InputAverage.Location = new System.Drawing.Point(616, 411);
+            this.InputAverage.Name = "InputAverage";
+            this.InputAverage.Size = new System.Drawing.Size(100, 30);
+            this.InputAverage.TabIndex = 5;
+            this.InputAverage.TextChanged += new System.EventHandler(this.InputAverage_TextChanged);
+            // 
+            // InputTotal
+            // 
+            this.InputTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputTotal.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.InputTotal.Location = new System.Drawing.Point(616, 339);
+            this.InputTotal.Name = "InputTotal";
+            this.InputTotal.Size = new System.Drawing.Size(100, 30);
+            this.InputTotal.TabIndex = 6;
+            this.InputTotal.TextChanged += new System.EventHandler(this.InputTotal_TextChanged);
+            // 
+            // Input2
+            // 
+            this.Input2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Input2.Location = new System.Drawing.Point(453, 123);
+            this.Input2.Name = "Input2";
+            this.Input2.Size = new System.Drawing.Size(100, 27);
+            this.Input2.TabIndex = 7;
+            this.Input2.TextChanged += new System.EventHandler(this.Input2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Font = new System.Drawing.Font("High Tower Text", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Strikeout))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Green;
+            this.button1.Location = new System.Drawing.Point(881, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 88);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.Font = new System.Drawing.Font("High Tower Text", 7.8F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Strikeout))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(881, 251);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 80);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "CLEAR";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Input3
+            // 
+            this.Input3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Input3.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.Input3.Location = new System.Drawing.Point(453, 193);
+            this.Input3.Name = "Input3";
+            this.Input3.Size = new System.Drawing.Size(100, 27);
+            this.Input3.TabIndex = 11;
+            this.Input3.TextChanged += new System.EventHandler(this.Input3_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label5.Font = new System.Drawing.Font("Goudy Stout", 10.2F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(89, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(156, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "English";
+            // 
+            // Input4
+            // 
+            this.Input4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Input4.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.Input4.Location = new System.Drawing.Point(453, 263);
+            this.Input4.Name = "Input4";
+            this.Input4.Size = new System.Drawing.Size(100, 27);
+            this.Input4.TabIndex = 13;
+            this.Input4.TextChanged += new System.EventHandler(this.Input4_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label6.Font = new System.Drawing.Font("Goudy Stout", 10.2F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(40, 265);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(264, 23);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Programming";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1137, 492);
+            this.Controls.Add(this.Input4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Input3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Input2);
+            this.Controls.Add(this.InputTotal);
+            this.Controls.Add(this.InputAverage);
+            this.Controls.Add(this.Input1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "Form1";
+            this.Text = "Grades Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Input1;
+        private System.Windows.Forms.TextBox InputAverage;
+        private System.Windows.Forms.TextBox InputTotal;
+        private System.Windows.Forms.TextBox Input2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox Input3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Input4;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basic_Grade_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float v1, v2, v3, v4, total, avg;
+            v1 = float.Parse(Input1.Text);
+            v2 = float.Parse(Input2.Text);
+            v3 = float.Parse(Input3.Text);
+            v4 = float.Parse(Input4.Text);
+            total = v1+v2+ v3 + v4;
+            avg = total / 4;
+
+            InputTotal.Text = "" + total;
+            InputAverage.Text = "" + avg;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Input1.Text = "";
+            Input2.Text = "";
+            Input3.Text = "";
+            Input4.Text = "";
+            InputTotal.Text = "";
+            InputAverage.Text = "";
+            Input1.Focus();
+        }
+
+        private void Input1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Input2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Input3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Input4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InputTotal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InputAverage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
